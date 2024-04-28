b395f58309f07ddca6d808e0d1ab4fb67e41a471
Jasmeet Singh <jasmeetsinghworking@gmail.com>
2024-04-28 21:07:25 +0530

    Complete Explore-Menu

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./Home.module.css";
 import Header from '../../components/Header/Header';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 
 const Home = () => {
+
+  const [category, setCategory] = useState("All");
   return (
     <div>
       <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   )
 }
